fd79aaddec3e8e3136dcf71833d55c1c0b41a844
Katie Struthers <99215338+katiestruthers@users.noreply.github.com>
2024-06-07 08:40:21 -0500

    setup react, tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Credits } from './components/Credits';
+
+function App() {
+  return (
+    <div className="App">
+      <Credits />
+    </div>
+  );
+};
+
+export default App;
